9a1de582eddafc2bb795ac1fea3848af1e68b56f
sam_lycett@hotmail.com <sam_lycett@hotmail.com@815ddd88-bb73-0148-3f23-46d696250ca4>
2013-09-16 11:25:14 +0000

    SJL - 16 Sept 2013 - added options for genetic distance calculation in ClusterPicker_command.jar - original option ("gap") is set as the default. Options are: gap - ignore difference between -,~,or,n and any other character, else count the differences valid - only count differences between valid nucleotides i.e. a, c, g, t abs - count all character differences ambiguity - ignore differences between -,~,n and anything, also do not count differences between ambiguities and compatible nucleotides (e.g. a vs r is not a difference)

--- trunk/src/clusterPicking/BasicSequence.java
+++ trunk/src/clusterPicking/BasicSequence.java
@@ -1,5 +1,7 @@
 package clusterPicking;
 
+import java.util.Arrays;
+
 /**
  *  class to represent a basic sequence
  *  Copyright (C) 2013  Samantha Lycett
@@ -27,13 +29,21 @@ package clusterPicking;
  * @version 26 Sept 2011
  * @version 3  Oct  2011 - allow differences to be measured as: absolute (abs), ignoring gaps only (gap) or valid only (a,c,t,g)
  * @vesrion 4  July 2012 - added String, char[] constructor
+ * @version 12 Sept 2013 - added ambiguity codes (optional)
  */
 public class BasicSequence {
 	
 	// class variable
 	// protected static boolean useAbs = false;
-	protected static String  differenceType = "gap";		// choices are abs, gap, valid
+	protected static String  differenceType = "gap";		// choices are abs, gap, valid, ambiguity
 	
+	public static void setDifferenceType(String dt) {
+		if (dt.equals("gap") || (dt.equals("abs") || (dt.equals("valid") || (dt.equals("ambiguity"))))) {
+			differenceType = dt;
+		} else {
+			System.out.println("BasicSequence.setDifferenceType: sorry cannot understand "+dt);
+		}
+	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -141,8 +151,22 @@ public class BasicSequence {
 		return g;
 	}
 	
+	/**
+	 * returns true is the site contains an IUPAC Ambiguity code
+	 * @param i
+	 * @return
+	 */
+	public boolean isAmbiguity(int i) {
+		char site = charSeq[i];
+		boolean a = ( 	( site == 'm') || ( site == 'r') || ( site == 'w') || ( site == 's') || ( site == 'y') || ( site == 'k') || 
+						( site == 'v') || ( site == 'h') || ( site == 'd') || ( site == 'b') || ( site == 'n')  );
+		return a;
+	}
+	
 	///////////////////////////////////////////////////////////////////////
 	// comparison methods
+	
+
 	
 	/**
 	 * calculates the raw character difference between the sequences, does not check for gaps etc
@@ -167,7 +191,7 @@ public class BasicSequence {
 	}
 	
 	/**
-	 * calculates the difference between this sequence and the input sequence, but only counts valid sites, i.e. a, c, t, g
+	 * calculates the difference between this sequence and the input sequence, but only counts valid sites, i.e. a, c, t, g (ignores gaps and ambs)
 	 * @param b
 	 * @return
 	 */
@@ -192,7 +216,7 @@ public class BasicSequence {
 	}
 	
 	/**
-	 * calculates the difference between this sequence and the input sequence, but only counts if neither is a gap
+	 * calculates the difference between this sequence and the input sequence, but only counts if neither is a gap (ambs scored as differences, gaps are ignored)
 	 * @param b
 	 * @return
 	 */
@@ -216,6 +240,72 @@ public class BasicSequence {
 		return count;
 	}
 	
+	
+	/**
+	 * returns true if the ambiguious site matches the unambiguous site, or the ambiguous site is -, ~, or n
+	 */
+	private boolean ambiguityMatch(char site, char amb) {
+		
+		//a: m, r, w, v, h, d, n
+		//c: m, s, y, v, h, b, n
+		//g: r, s, k, v, d, b, n
+		//t: w, y, k, h, d, b, n
+		
+		if ( (amb == 'n') || (amb == '-') || (amb == '~') || (amb == site) ) {
+			return true;
+		} else {
+		
+			if (site == 'a') {
+				return ( (amb=='m') || (amb=='r') || (amb=='w') || (amb=='v') || (amb=='h') || (amb=='d') );
+			} else if ( site=='c' ) {
+				return ( (amb=='m') || (amb=='s') || (amb=='y') || (amb=='v') || (amb=='h') || (amb=='b'));
+			} else if ( site=='g' ) {
+				return ( (amb=='r') || (amb=='s') || (amb=='k') || (amb=='v') || (amb=='d') || (amb=='b'));
+			} else if ( site=='t') {
+				return ( (amb=='w') || (amb=='y') || (amb=='k') || (amb=='h') || (amb=='d') || (amb=='b'));
+			} else {
+				return false;
+			}
+		
+		}
+		
+	}
+	
+	/**
+	 * calculates the difference between this sequence and the input sequence b, but doesnt count matching ambiguities (gaps are ignored, ambiguities are processed)
+	 * @param b
+	 * @return
+	 */
+	public int ambiguityDifference(BasicSequence b) {
+		int count = 0;
+		int len   = charSeq.length;
+		
+		if (b.charSeq.length < len) {
+			len	  = b.charSeq.length;
+		}
+		
+		for (int i = 0; i < len; i++) {
+			if ( isValid(i) ) {
+				if ( !ambiguityMatch( charSeq[i], b.charSeq[i]) ) {
+					count++;
+				}
+			} else if ( b.isValid(i) ) {
+				if ( !ambiguityMatch( b.charSeq[i], charSeq[i]) ) {
+					count++;
+				}
+			} else {
+				// if neither are valid then count as difference if different ambiguity
+				if ( isAmbiguity(i) && b.isAmbiguity(i) ) {
+					if ( charSeq[i] != b.charSeq[i] ) {
+						count++;
+					}
+				}
+			}
+		}
+		
+		return count;
+	}
+	
 	public int difference(BasicSequence b) {
 		if (differenceType.equals("abs")) {
 			return absDifference(b);
@@ -223,6 +313,8 @@ public class BasicSequence {
 			return noGapDifference(b);
 		} else if (differenceType.equals("valid")) {
 			return validDifference(b);
+		} else if (differenceType.equals("ambiguity")) {
+			return ambiguityDifference(b);
 		} else {
 			System.out.println("BasicSequence:difference\tcant understand "+differenceType);
 			return -1;
@@ -299,6 +391,36 @@ public class BasicSequence {
 		return ( (double)count / (double)len ) ;
 	}
 	
+	public double ambiguityFractDifference(BasicSequence b) {
+		int count = 0;
+		int len   = charSeq.length;
+		
+		if (b.charSeq.length < len) {
+			len	  = b.charSeq.length;
+		}
+		
+		for (int i = 0; i < len; i++) {
+			if ( isValid(i) ) {
+				if ( !ambiguityMatch( charSeq[i], b.charSeq[i]) ) {
+					count++;
+				}
+			} else if ( b.isValid(i) ) {
+				if ( !ambiguityMatch( b.charSeq[i], charSeq[i]) ) {
+					count++;
+				}
+			} else {
+				// if neither are valid then count as difference if different ambiguity
+				if ( isAmbiguity(i) && b.isAmbiguity(i) ) {
+					if ( charSeq[i] != b.charSeq[i] ) {
+						count++;
+					}
+				}
+			}
+		}
+		
+		return ( (double)count / (double)len ) ;
+	}
+	
 	public double fractDifference(BasicSequence b) {
 		if (differenceType.equals("abs")) {
 			return absFractDifference(b);
@@ -306,11 +428,47 @@ public class BasicSequence {
 			return noGapFractDifference(b);
 		} else if (differenceType.equals("valid")) {
 			return validFractDifference(b);
+		} else if (differenceType.equals("ambiguity")) {
+			return ambiguityFractDifference(b);
 		} else {
 			System.out.println("BasicSequence:fractDifference\tcant understand "+differenceType);
 			return -1;
 		}
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(charSeq);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	/*
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof BasicSequence)) {
+			return false;
+		}
+		BasicSequence other = (BasicSequence) obj;
+		if (!Arrays.equals(charSeq, other.charSeq)) {
+			return false;
+		}
+		return true;
+	}
+	*/
 	
 	/**
 	 * returns true if the sequence names match - not the sequence contents !
@@ -328,5 +486,10 @@ public class BasicSequence {
 		}
 		
 	}
+	
+	
+	
+	
+	
 	
 }
