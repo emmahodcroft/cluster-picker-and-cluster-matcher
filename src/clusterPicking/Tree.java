41f121c182f11c9f95598084c141a3aefd9acf9c
emmahodcroft <freezefire666@yahoo.com>
2015-09-15 10:46:07 +0100

    Update Rooting/BS/Poly fix
    Now errors if polytomy detected. Can handle missing branch length and bootstrap info. Can handle different types of rooting. Message update works now when processing, and spinning bar is displayed. 'GO' button disabled while processing and if error.
    If no BS and both child BL are 0, BS is set to 100 (assumes identical sequences) and warning displayed.

--- src/clusterPicking/Tree.java
+++ src/clusterPicking/Tree.java
@@ -31,23 +31,28 @@ import java.util.regex.*;
  * @version 27 Sept 2011 - accepts | and / in tip names also
  * @version 3  Oct  2011 - accepts - in tip names also
  * @version 8  Nov  2011 - changed support regex
+ * @version 15 Sept	2015 - Now errors if polytomy detected. Can handle missing branch length and bootstrap info. Can handle different types of rooting. Message update works now when processing, and spinning bar is displayed. 'GO' button disabled while processing and if error.
  */
 public class Tree {
 
 	List<Node> 	nodes;
 	String 		trLine;
 	
-	String  branchLengthRegex 	= "(\\-)?+[0-9]+(\\.[0-9eE\\-]+)?";
+	String  branchLengthRegex 	= "(\\-)?+[0-9]+(\\.[0-9eE\\-]+)?";   //(-)0-9+ (.0-9eE-0-9)
+String bootstrapRegex = "((\\-)?+[0-9]+(\\.[0-9eE\\-]+)?)?"; //ebh 
 	String 	branchDelim			= ":";
 	//String  nodeRegex   	  	= "[a-zA-Z_0-9\\.]+" +   branchDelim + branchLengthRegex;
 	String  nodeRegex   	  	= "[a-zA-Z_0-9\\.\\|/\\-]+" +   branchDelim + branchLengthRegex;
 	//String  supportRegex		= "[0-9]+\\.[0-9]+" + branchDelim + branchLengthRegex;
-	String  supportRegex		= branchLengthRegex + branchDelim + branchLengthRegex;
+//	String  supportRegex		= branchLengthRegex + branchDelim + branchLengthRegex; //ebh
+String supportRegex = bootstrapRegex + branchDelim + branchLengthRegex; //ebh
 	Pattern nodePattern 	  	= Pattern.compile( nodeRegex  );
 	//Pattern supportPattern	= Pattern.compile( supportRegex );
 	//Pattern sibPattern  	  	= Pattern.compile( "\\("+nodeRegex+","+nodeRegex+"\\)");
 	Pattern sibSupportPattern	= Pattern.compile( "\\(" + nodeRegex + "," + nodeRegex + "\\)" + supportRegex );
 	Pattern triPattern			= Pattern.compile( "\\(" + nodeRegex + "," + nodeRegex + "," + nodeRegex + "\\)");
+	Pattern polyPattern 		= Pattern.compile( "\\(" + "("+nodeRegex+",){2,}" + nodeRegex + "\\)");  //{n,} -match at least n times
+Pattern biPattern = Pattern.compile( "\\(" + nodeRegex + "," + nodeRegex + "\\)"); //ebh
 	
 	
 	public Tree() {
@@ -61,7 +66,8 @@ public class Tree {
 		Matcher elMatcher = sibSupportPattern.matcher(tempTr);
 		
 		while (elMatcher.find()) {
-			String txt	  	= elMatcher.group();
+//System.out.println("\t "+tempTr); //ebh
+			String txt	  	= elMatcher.group(); //matches sibsupport pattern, returns 1st match
 		
 			String txt2 	= txt.replace("(", "");
 			txt2 			= txt2.replace(")", ",");
@@ -71,6 +77,8 @@ public class Tree {
 			// this is the parent
 			Node parent = new Node();
 			parent.setSupportBranchLength( els[els.length-1] );
+			
+			Node[] childs = new Node[2];//ebh - to hold br lengths of children to check if identical seqs
 			
 			// the first two elements are nodeName:branchLength
 			// these are children
@@ -93,6 +101,17 @@ public class Tree {
 					parent.addChild(n);
 					
 					nodes.add(n);
+				}
+				childs[i] = n;
+			}
+			
+			String[] parSupp = els[els.length-1].split(":");
+			if(parSupp[0].length() == 0){   //if no bootstrap value, check lengths of children!
+				if(childs[0].getBranchLength() == 0 && childs[1].getBranchLength() == 0){
+					//if both child branch length is zero - set parent support to 100 - identical seqs
+					parent.setSupport(100.0);
+					System.out.println("**WARNING**: No support value and children have branch length of 0 - identical sequences likely. "
+							+ "\n\t**Setting support to 100% to allow cluster picking of this pair.**");
 				}
 			}
 			
@@ -170,6 +189,59 @@ public class Tree {
 		
 	}
 	
+	//ebh all below function
+	/**
+	 * use this to do the last bi furcation - and make up a root node -- in cases where tree is re-rooted for example, and has one
+	 * node at root without bootstrap/length for connection to rest of tree!
+	 * @param tempTr
+	 * @return
+	 */
+	private String processBifucation(String tempTr) {
+		Matcher elMatcher = biPattern.matcher(tempTr);
+		
+		if (elMatcher.find()) {
+			
+			Node rootNode		= new Node();
+			rootNode.setName("Root");
+			rootNode.setSupport(0);
+			rootNode.setBranchLength(0);
+			
+			String txt 			= elMatcher.group();
+			Matcher nodeMatcher = nodePattern.matcher(txt);
+			
+			while (nodeMatcher.find()) {
+				String el = nodeMatcher.group();
+				Node n = new Node();
+				n.setNameBranchLength( el );
+				
+				if (nodes.contains(n)) {
+					// nodes list already has this element, it will have name, support and branchlength
+					int j 	= nodes.indexOf(n);
+					Node n2 = nodes.get(j);
+					
+					// add parent to this node
+					n2.setParent(rootNode);
+					
+					// add child to parent
+					rootNode.addChild(n2);
+				} else {
+					n.setParent(rootNode);
+					rootNode.addChild(n);
+					
+					nodes.add(n);
+				}
+				
+			}
+			
+			nodes.add(rootNode);
+			String newTxt = rootNode.getName()+rootNode.delim+rootNode.getBranchLength();
+			tempTr		  = tempTr.replace(txt, newTxt);
+		}
+		
+		return (tempTr);
+		
+	}
+	
 	public void readTree(String trLine) {
 		
 		this.trLine 	  = trLine;
@@ -181,8 +253,18 @@ public class Tree {
 		
 		//System.out.println("Temp tree = "+tempTr);
 		
+		//check to see if it contains any polytomies:
+		Matcher elMatcher = polyPattern.matcher(tempTr);
 		
+		if (elMatcher.find()) {
+			System.out.println("Tip polytomy found: "+elMatcher.group());
+			System.out.println("***ERROR*** TIP POLYTOMY FOUND!!!");
+			throw new UnsupportedOperationException("ERROR: A tip polytomy was found (possibly due to identical sequences). The ClusterMatcher cannot handle tip or internal polytomies. Please remove the polytomy from your tree and re-run the ClusterPicker.\n...Exiting...");
+		}
+		
+//System.out.println();  //ebh
 		while (diffLength > 0) {
+//System.out.println(tempTr); //ebh
 		
 			int		origLength	= tempTr.length();			
 			tempTr 			  	= processTree(tempTr);
@@ -191,7 +273,29 @@ public class Tree {
 			//System.out.println("Diff = "+diffLength);
 			//System.out.println("Temp tree = "+tempTr);
 		}
+		
+		//check again it hasn't stopped early because of a polytomy
+		//polytomies found here are internal - where one or more nodes in the polytomy
+		//are internal nodes (not tips)
+		elMatcher = polyPattern.matcher(tempTr);  //ensure has new tempTr
+		if (elMatcher.find()) {  //if find polytomy
+//			System.out.println("Internal polytomy found: "+elMatcher.group()); //this will show even if is root polytomy which is ok
+			//polytomy only bad at this stage if not at root 
+			//should only have 1 open parenthesis
+			Matcher m = Pattern.compile("\\(").matcher(tempTr);
+
+			int matches = 0;
+			while(m.find())
+			    matches++;
 			
+			//System.out.println("number of (: "+matches);
+			if(matches > 1){
+				System.out.println("***ERROR*** INTERNAL POLYTOMY FOUND!!!");
+				throw new UnsupportedOperationException("ERROR: An internal polytomy was found. The ClusterMatcher cannot handle tip or internal polytomies. Please remove the polytomy from your tree and re-run the ClusterPicker.\n...Exiting...");
+			}
+		}
+			
+		tempTr = processBifucation(tempTr);
 		// now do final part if necessary - the tree should be really short by now
 		tempTr = processTrifucation(tempTr);
 		//System.out.println("Temp tree = "+tempTr);
