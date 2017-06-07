7cefefbe5898a1c852812ff2ae2a27baab51e88c
emmahodcroft <freezefire666@yahoo.com>
2017-06-07 15:19:48 +0100

    Symbols in Sequence Names
    Log shows ClusterPicker version. Seq names can now contain {} symbols. Throws error if large parts of tree remain unparsed, tells user to check for symbols.

--- src/clusterPicking/ClusterPickerGUI.java
+++ src/clusterPicking/ClusterPickerGUI.java
@@ -18,7 +18,7 @@ import java.beans.PropertyChangeEvent;
  *  ClusterPickerGUI - 
  *  This is the GUI front end - see ClusterPicker.java for actual cluster picker implementation.
  *  Picks clusters from a phylogenetic tree and set of sequences according to bootstrap values and genetic distance.
- *  Copyright (C) 2016  Samantha Lycett
+ *  Copyright (C) 2017  Samantha Lycett
  *  
  *  This file is part of ClusterPicker.
  *
@@ -51,13 +51,14 @@ import java.beans.PropertyChangeEvent;
  *  also fixed message updating while running, and now displays little spinning line (EBH)
  * @version 19 Jan 2016 - GO Button is re-enabled after finished data processing so that users can change files/params
  * 	and re-run data. Manon used to do this with old version and says is good (variables zeroed out). (EBH)
- * @version 13 Apr  2016 - include full list of sequence name + cluster number as separate output file
- * @version 15 Apr  2016 - corrected full list output
+ * @version 13 Apr  2016 - include full list of sequence name + cluster number as separate output file SL
+ * @version 15 Apr  2016 - corrected full list output SL
+ * @version 7 Jun 2017 - EBH - updated to clarify that genetic distance should be in % (4.5, not 0.045)
  */
 public class ClusterPickerGUI {
 	
 	protected String appName   = "ClusterPicker";
-	protected String version   = "1.2.4 (15 Apr 2016)"; //"1.2.1 (19 Jan 2016)"; //1.0 (4 Sept 2013)";	//"1.0 (10 June 2013)";	//"1.0 (23 July 2012)";
+	protected String version   = "1.2.5 (7 Jun 2017)"; //1.2.4 (15 Apr 2016)"; //"1.2.1 (19 Jan 2016)"; //1.0 (4 Sept 2013)";	//"1.0 (10 June 2013)";	//"1.0 (23 July 2012)";
 	protected String author	   = "Dr. S. J. Lycett, Dr. E. B. Hodcroft, Dr. M. Ragonnet-Cronin";
 	protected String institute = "University of Edinburgh";
 	protected String citation  = "Ragonnet-Cronin et al Automated Analysis of Phylogenetic Clusters";
@@ -399,7 +400,7 @@ public class ClusterPickerGUI {
 		
 		
 		// for genetic distance threshold
-		JLabel geneticThresLabel = new JLabel("Genetic Distance Threshold:");
+		JLabel geneticThresLabel = new JLabel("Genetic Distance Threshold (%):");
 		geneticThresLabel.setMinimumSize(labelBoxSize);
 		geneticThresLabel.setHorizontalAlignment(JLabel.RIGHT);
 		geneticThresLabel.setVerticalAlignment(JLabel.CENTER);
@@ -1409,7 +1410,7 @@ public class ClusterPickerGUI {
 		//  your GUI creation code should be placed on the Event Dispatch Thread (EDT). 
 		//  This will prevent potential race conditions that could lead to deadlock
 		
-		System.out.println("ClusterPicker Copyright (C) 2016 Samantha Lycett");
+		System.out.println("ClusterPicker Copyright (C) 2017 Samantha Lycett");
 		System.out.println("This program comes with ABSOLUTELY NO WARRANTY");
 		System.out.println("This is free software, and you are welcome to redistribute it under certain conditions");
 		System.out.println("See GNU GPLv3 for details http://www.gnu.org/licenses/gpl-3.0.txt");
